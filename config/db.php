2d2f31e705367e559a276a2ebaa41d8cd60681b7
lndryu <leandro.lojero23@gmail.com>
2026-05-29 00:17:14 +0800

    changes

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
-$host = '127.0.0.1';
-$port = 3308;
-$db   = 'csedb';
-$user = 'root';
-$pass = '';
+$host = getenv('DB_HOST') ?: '127.0.0.1';
+$port = getenv('DB_PORT') ?: 3308;
+$db   = getenv('DB_NAME') ?: 'csedb';
+$user = getenv('DB_USER') ?: 'root';
+$pass = getenv('DB_PASS') ?: '';
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
