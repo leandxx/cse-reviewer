23abe6483b117f22cd0b525d20544c2ff0b17e9e
lndryu <leandro.lojero23@gmail.com>
2026-05-29 01:03:11 +0800

    changes

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
-$host = getenv('DB_HOST') ?: '127.0.0.1';
+$host = getenv('DB_HOST') ?: 'mysql.railway.internal';
 $port = getenv('DB_PORT') ?: 3308;
-$db   = getenv('DB_NAME') ?: 'csedb';
+$db   = getenv('DB_NAME') ?: 'railway';
 $user = getenv('DB_USER') ?: 'root';
-$pass = getenv('DB_PASS') ?: '';
+$pass = getenv('DB_PASS') ?: 'qjpEqXyOhsopBMQQcFANMSzhEPPjbGQj';
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=utf8", $user, $pass);
